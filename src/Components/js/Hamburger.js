5f722c3d9d07adf884fd4349c2119524734898ee
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-03-24 11:42:28 +0100

    Add customized menu for touch and mouse devices

--- src/Components/js/Hamburger.js
+++ src/Components/js/Hamburger.js
@@ -3,11 +3,17 @@ import './../styles/Hamburger.css'
 
 export default ()=>{
   const [isOpen, setIsOpen] = useState(false);
+  const [currentTab, setCurrentTab] = useState("Home")
 
   return (
     <div className={isOpen? "hamburger-cont menu-open" : "hamburger-cont"}>
-        <button className="menu-button" onClick={()=>setIsOpen(!isOpen)}></button>
-        <div className="hamburger"><div></div></div>
+        <div className="hamburger"onClick={()=>setIsOpen(!isOpen)}><div></div></div>
+        <div className="menu">
+          <ul>
+            <li className={currentTab==="Home"? "current" : "other"} onClick={()=>setCurrentTab("Home")}>Home</li>
+            <li className={currentTab==="Tech"? "current" : "other"} onClick={()=>setCurrentTab("Tech")}>Tech</li>
+          </ul>
+        </div>
     </div>
   );
 }
