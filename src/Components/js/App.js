d8dff7b7f7c7093bf60e8c553eae39fb2277eace
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-03-24 22:57:42 +0100

    Add content

--- src/Components/js/App.js
+++ src/Components/js/App.js
@@ -1,29 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import './../styles/App.css';
 import {Parallax, Hamburger} from '.'
 
+
+export const PageContext = createContext(null);
+
+
 export default ()=>{
 
+  const [page,setPage] = useState("Home");
   const [posts, setPosts] = useState([]);
+
+  const paths = {
+    "Home":"posts/front.json",
+    "AI":"posts/ai.json",
+    "Sprint":"posts/sprint.json",
+    "BAT Streaming":"posts/streaming.json",
+    "ShowTeam":"posts/showteam.json",
+    "NTNUI Salsa":"posts/ntnuisalsa.json",
+    "Sosialt":"posts/salsasosialt.json",
+    "Volleyball":"posts/volleyball.json"
+  }
+
+
   useEffect(() => {
-    fetch('posts/front.json')
+    if (paths[page]){
+      fetch(paths[page])
     .then((res)=>res.json())
     .then((json)=>{
       setPosts(json);
     });
-  }, []);
+    }
+  }, [page]);
 
   return (
-    <div className="App">
-      <Hamburger/>
-      <div className="content">
-      {posts.map((post,index)=>{
-        const imageSide = (index % 2 === 0 ? "left" : "right")
-        const imageURL = '/img/' + post.imageName
-          return <Parallax imageSide={imageSide} title={post.title} text={post.text} imageURL={imageURL}/>
-        })
-      }
+    <PageContext.Provider value={{page,setPage}}>
+      <div className="App">
+        <Hamburger/>
+        <div className="content">
+          {posts.map((post,index)=>{
+            const imageSide = (index % 2 === 0 ? "left" : "right")
+            const imageURL = '/img/' + post.imageName
+            return <Parallax imageSide={imageSide} title={post.title} text={post.text} imageURL={imageURL}/>
+          })}
+        </div>
       </div>
-    </div>
+    </PageContext.Provider>
   );
 }
--- src/Components/js/HamburgerMenu.js
+++ src/Components/js/HamburgerMenu.js
@@ -1,49 +1,49 @@
-import React, { useState} from 'react';
+import React, { useState, useContext} from 'react';
 import './../styles/HamburgerMenu.css'
+import { PageContext } from './App'
 
 export default ()=>{
-  const [currentTab, setCurrentTab] = useState("Home")
-
-  return (
-    <div className="menu">
-        <div className="category">
-            <div className="header-container">
-                <h3>Generelt</h3>
+    const {page, setPage} = useContext(PageContext);
+    return (
+        <div className="menu">
+            <div className="category">
+                <div className="header-container">
+                    <h3>Generelt</h3>
+                </div>
+            <ul id="General">
+                <li className={page==="Home"? "current" : "other"} onClick={()=>setPage("Home")}>Hjem</li>
+            </ul>
             </div>
-        <ul id="General">
-            <li className={currentTab==="Home"? "current" : "other"} onClick={()=>setCurrentTab("Home")}>Hjem</li>
-        </ul>
-        </div>
-        <div className="category">
-            <div className="header-container">
-                <h3>Teknologi</h3>
+            <div className="category">
+                <div className="header-container">
+                    <h3>Teknologi</h3>
+                </div>
+            <ul className="Tech">
+                <li className={page==="AI"? "current" : "other"} onClick={()=>setPage("AI")}>Kunstig intelligens</li>
+                <li className={page==="Sprint"? "current" : "other"} onClick={()=>setPage("Sprint")}>NTNUI Sprint</li>
+                <li className={page==="BAT Streaming"? "current" : "other"} onClick={()=>setPage("BAT Streaming")}>BAT Streaming</li>
+            </ul>
             </div>
-        <ul className="Tech">
-            <li className={currentTab==="AI"? "current" : "other"} onClick={()=>setCurrentTab("AI")}>Kunstig intelligens</li>
-            <li className={currentTab==="Sprint"? "current" : "other"} onClick={()=>setCurrentTab("Sprint")}>NTNUI Sprint</li>
-            <li className={currentTab==="BAT Streaming"? "current" : "other"} onClick={()=>setCurrentTab("BAT Streaming")}>BAT Streaming</li>
-        </ul>
-        </div>
-        <div className="category">
-            <div className="header-container">
-                <h3>Dans</h3>
+            <div className="category">
+                <div className="header-container">
+                    <h3>Dans</h3>
+                </div>
+            <ul className="Dance">
+                <li className={page==="ShowTeam"? "current" : "other"} onClick={()=>setPage("ShowTeam")}>SalsaNor Mens Show Team</li>
+                <li className={page==="NTNUI Salsa"? "current" : "other"} onClick={()=>setPage("NTNUI Salsa")}>NTNUI Salsa</li>
+                <li className={page==="Sosialt"? "current" : "other"} onClick={()=>setPage("Sosialt")}>Sosialt</li>
+            </ul>
             </div>
-        <ul className="Dance">
-            <li className={currentTab==="ShowTeam"? "current" : "other"} onClick={()=>setCurrentTab("ShowTeam")}>SalsaNor Mens Show Team</li>
-            <li className={currentTab==="NTNUI Salsa"? "current" : "other"} onClick={()=>setCurrentTab("NTNUI Salsa")}>NTNUI Salsa</li>
-            <li className={currentTab==="Sosialt og reising"? "current" : "other"} onClick={()=>setCurrentTab("Sosialt og reising")}>Sosialt og reising</li>
-        </ul>
-        </div>
-        <div className="category">
-            <div className="header-container">
-                <h3>Hobbyer</h3>
+            <div className="category">
+                <div className="header-container">
+                    <h3>Hobbyer</h3>
+                </div>
+            <ul className="Hobbies">
+                <li className={page==="Volleyball"? "current" : "other"} onClick={()=>setPage("Volleyball")}>Volleyball</li>
+            </ul>
             </div>
-        <ul className="Hobbies">
-            <li className={currentTab==="Volleyball"? "current" : "other"} onClick={()=>setCurrentTab("Volleyball")}>Volleyball</li>
-        </ul>
         </div>
-    </div>
-  );
+    );
 }
 
 
