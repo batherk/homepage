fb81be93944bbf12f93b8baac3f7bc8b92939772
Bjørn Are Therkelsen <ba.therk@outlook.com>
2021-08-11 16:44:12 +0900

    Translate menu names

--- src/Components/js/HamburgerMenu.js
+++ src/Components/js/HamburgerMenu.js
@@ -9,35 +9,33 @@ export default ()=>{
         <div className="menu">
             <div className="category">
                 <div className="header-container">
-                    <h3>Generelt</h3>
+                    <h3>General</h3>
                 </div>
             <ul id="General">
-                <li className={location.pathname==="/"? "current" : "other"} onClick={()=>history.push("/")}>Hjem</li>
+                <li className={location.pathname==="/"? "current" : "other"} onClick={()=>history.push("/")}>Home</li>
             </ul>
             </div>
             <div className="category">
                 <div className="header-container">
-                    <h3>Teknologi</h3>
+                    <h3>Technology</h3>
                 </div>
             <ul className="Tech">
-                <li className={location.pathname==="/ai"? "current" : "other"} onClick={()=>history.push("/ai")}>Kunstig intelligens</li>
                 <li className={location.pathname==="/sprint"? "current" : "other"} onClick={()=>history.push("/sprint")}>NTNUI Sprint</li>
                 <li className={location.pathname==="/streaming"? "current" : "other"} onClick={()=>history.push("/streaming")}>BAT Streaming</li>
             </ul>
             </div>
             <div className="category">
                 <div className="header-container">
-                    <h3>Dans</h3>
+                    <h3>Dance</h3>
                 </div>
             <ul className="Dance">
                 <li className={location.pathname==="/showteam"? "current" : "other"} onClick={()=>history.push("/showteam")}>SalsaNor Men Show Team</li>
                 <li className={location.pathname==="/salsa"? "current" : "other"} onClick={()=>history.push("/salsa")}>NTNUI Salsa</li>
-                <li className={location.pathname==="/travelling"? "current" : "other"} onClick={()=>history.push("/travelling")}>Reising</li>
             </ul>
             </div>
             <div className="category">
                 <div className="header-container">
-                    <h3>Hobbyer</h3>
+                    <h3>Hobbies</h3>
                 </div>
             <ul className="Hobbies">
                 <li className={location.pathname==="/volleyball"? "current" : "other"} onClick={()=>history.push("/volleyball")}>Volleyball</li>
